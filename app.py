f38112d64dc36ce36d3eaf08dafcb075d8db6676
i-hayato <i-hayato@freemind.co.jp>
2025-06-18 16:34:20 +0900

    to gunicorn

--- app.py
+++ app.py
@@ -80,10 +80,10 @@ def get_report():
     })
 
 
-
+'''
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # デフォルト10000
-    # app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=port)
+'''
